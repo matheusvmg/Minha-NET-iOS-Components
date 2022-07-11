1340a840c4c587f2d54b6154e50c437049443534
Matheus Vinícius <matheusvmg@gmail.com>
2022-07-11 11:08:50 -0300

    rename the CardView component file and change example viewController

--- Example/Minha-NET-iOS-Components-Example/ViewController.swift
+++ Example/Minha-NET-iOS-Components-Example/ViewController.swift
@@ -14,21 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         self.view.addSubview(card)
-        self.view.addSubview(card2)
         setupConstraints()
     }
 
     private lazy var card: CardView = {
-        let cardInformation = CardContent(icon: "wifi", title: "Boot", subtitle: "Uptime", description: "Faz mais de 10 dias que você não reinicia seu modem. O que acha da gente fazer um boot?", hasButton: true, buttonTitle: "Agendar visita técnica", isRounded: false)
         let card = CardView()
-        card.cardInformation = cardInformation
-        return card
-    }()
-    
-    private lazy var card2: CardView = {
-        let cardInformation = CardContent(icon: "wifi", title: "Boot", subtitle: "Uptime", description: "Faz mais de 10 dias que você não reinicia seu modem. O que acha da gente fazer um boot?", hasButton: true, buttonTitle: "Agendar visita técnica", isRounded: false)
-        let card = CardView()
-        card.cardInformation = cardInformation
+        card.cardInformation = CardContent(icon: "wifi", title: "Título", subtitle: "Subtítulo", description: "description", hasButton: true, buttonTitle: "título do botão", isRounded: true)
         return card
     }()
     
@@ -36,11 +27,7 @@ class ViewController: UIViewController {
         NSLayoutConstraint.activate([
             card.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50),
             card.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
-            card.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30),
-            
-            card2.topAnchor.constraint(equalTo: card.bottomAnchor, constant: 50),
-            card2.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
-            card2.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30)
+            card.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30)
         ])
     }
 
