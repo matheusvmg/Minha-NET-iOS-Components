9d7b771d009682ab8b8465817441199c5f409d1e
Matheus Vinícius <matheusvmg@gmail.com>
2022-07-11 01:20:00 -0300

    public init in CardContent struct

--- Sources/Minha-NET-iOS-Components/Components/CardContent.swift
+++ Sources/Minha-NET-iOS-Components/Components/CardContent.swift
@@ -8,6 +8,16 @@
 import Foundation
 
 public struct CardContent {
+    public init(icon: String?, title: String, subtitle: String?, description: String, hasButton: Bool, buttonTitle: String?, isRounded: Bool) {
+        self.icon = icon
+        self.title = title
+        self.subtitle = subtitle
+        self.description = description
+        self.hasButton = hasButton
+        self.buttonTitle = buttonTitle
+        self.isRounded = isRounded
+    }
+    
     public let icon: String?
     public let title: String
     public let subtitle: String?
@@ -15,6 +25,4 @@ public struct CardContent {
     public let hasButton: Bool
     public let buttonTitle: String?
     public let isRounded: Bool
-    
-    public init() {}
 }
