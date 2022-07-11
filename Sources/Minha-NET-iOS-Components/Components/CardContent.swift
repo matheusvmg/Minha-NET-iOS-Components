7befe5048a5ef93091bb242380feec0fe407a9d8
Matheus Vinícius <matheusvmg@gmail.com>
2022-07-11 01:37:31 -0300

    Refactor CardContent struct model

---
+++ Sources/Minha-NET-iOS-Components/Components/CardContent.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Matheus Góes on 11/07/22.
+//
+
+import Foundation
+
+public struct CardContent {
+    let icon: String?
+    let title: String
+    let subtitle: String?
+    let description: String
+    let hasButton: Bool
+    let buttonTitle: String?
+    let isRounded: Bool
+    
+    public init(icon: String?, title: String, subtitle: String?, description: String, hasButton: Bool, buttonTitle: String?, isRounded: Bool) {
+        self.icon = icon
+        self.title = title
+        self.subtitle = subtitle
+        self.description = description
+        self.hasButton = hasButton
+        self.buttonTitle = buttonTitle
+        self.isRounded = isRounded
+    }
+}
